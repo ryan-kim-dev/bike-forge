ec935839f75a19bfbbe2dc0df7c5524d49268894
ryan-kim-dev <ryankim.h.dev@gmail.com>
2025-12-18 20:50:09 +0900

    chore: 모델링을 로컬 대신 r2 개발용 url에서 불러오도록 변경

--- src/app/components/cub.tsx
+++ src/app/components/cub.tsx
@@ -2,7 +2,9 @@
 import { useGLTF } from '@react-three/drei';
 
 export default function Cub() {
-  const { scene } = useGLTF('/models/scene.gltf');
+  const { scene } = useGLTF(
+    `${process.env.NEXT_PUBLIC_R2_PUBLIC_BASE_URL}/models/maker/honda/series/super-cub/c70/gen-1/1968/jp/standard/runtime/cub.gltf`
+  );
 
   return <primitive object={scene} />;
 }
