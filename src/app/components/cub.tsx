b6269690a08ccbc31fe9678cdcba9378fb0bc9c6
ryan-kim-dev <ryankim.h.dev@gmail.com>
2025-12-19 18:38:14 +0900

    chore: 모델링을 런타임용 경량화 버젼으로 교체

--- src/app/components/cub.tsx
+++ src/app/components/cub.tsx
@@ -3,7 +3,7 @@ import { useGLTF } from '@react-three/drei';
 
 export default function Cub() {
   const { scene } = useGLTF(
-    `${process.env.NEXT_PUBLIC_R2_PUBLIC_BASE_URL}/models/maker/honda/series/super-cub/c70/gen-1/1968/jp/standard/runtime/cub.gltf`
+    `${process.env.NEXT_PUBLIC_R2_PUBLIC_BASE_URL}/models/maker/honda/series/super-cub/c70/gen-1/1968/jp/standard/runtime/cub-transformed.gltf`
   );
 
   return <primitive object={scene} />;
