15dc767222cadb00e32a5d57894d634bc0fe01d1
ryan-kim-dev <ryankim.h.dev@gmail.com>
2025-12-19 18:38:59 +0900

    feat: 모델링 loading ui 추가

--- src/app/components/viewer.tsx
+++ src/app/components/viewer.tsx
@@ -1,17 +1,25 @@
 'use client';
 import { Canvas } from '@react-three/fiber';
-import { Environment, OrbitControls } from '@react-three/drei';
+import { Loader, Environment, OrbitControls } from '@react-three/drei';
 import Cub from './cub';
+import { Suspense } from 'react';
 
 export default function Viewer() {
   return (
-    <Canvas camera={{ fov: 50, near: 0.05, far: 100, position: [0, 1.2, 3.2] }}>
-      <OrbitControls />
-      <ambientLight intensity={0.6} />
-      <directionalLight position={[5, 5, 5]} intensity={2.0} />
+    <>
+      <Canvas
+        camera={{ fov: 50, near: 0.05, far: 100, position: [0, 1.2, 3.2] }}
+      >
+        <Suspense fallback={null}>
+          <OrbitControls />
+          <ambientLight intensity={0.6} />
+          <directionalLight position={[5, 5, 5]} intensity={2.0} />
 
-      <Environment preset="city" />
-      <Cub />
-    </Canvas>
+          <Environment preset="city" />
+          <Cub />
+        </Suspense>
+      </Canvas>
+      <Loader />
+    </>
   );
 }
