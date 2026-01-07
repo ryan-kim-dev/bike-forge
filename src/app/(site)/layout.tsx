6a39d58ee05044ce6042ef8e3d75c45c88ce2a2e
ryan-kim-dev <ryankim.h.dev@gmail.com>
2026-01-07 20:29:28 +0900

    refactor: 레이아웃에서 불필요한 중복요소 삭제

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,15 +1,12 @@
 import type { ReactNode } from 'react';
 import Header from '@/components/layout/header';
 import Footer from '@/components/layout/footer';
-import Container from '@/components/layout/container';
 
 export default function SiteLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <Header />
-      <main>
-        <Container>{children}</Container>
-      </main>
+      {children}
       <Footer />
     </>
   );
